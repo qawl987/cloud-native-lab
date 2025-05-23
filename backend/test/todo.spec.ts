38fc8a457a9a0b47c515ab42c6327a0ee1b574d2
qawl987 <62208230+qawl987@users.noreply.github.com>
2025-05-23 12:43:10 +0000

    feat: add a simple v2 test

--- backend/test/todo.spec.ts
+++ backend/test/todo.spec.ts
@@ -23,4 +23,19 @@ describe('Todo API Testing', () => {
     const todos = JSON.parse(response.body)['todos']
     expect(todos).toStrictEqual([])
   })
+
+  // test('Given an empty array return from repo function, When send a GET request to /api/v2/todos, Then it should response an empty array', async () => {
+  //   // arrange: stub the repo function to return an empty array
+  //   vi.spyOn(TodoRepo, 'findAllTodos').mockImplementation(async () => [])
+
+  //   // act: send a GET request to /api/v1/todos
+  //   const response = await server.inject({
+  //     method: 'GET',
+  //     url: '/api/v2/todos'
+  //   })
+
+  //   // assert: response should be an empty array
+  //   const todos = JSON.parse(response.body)['todos']
+  //   expect(todos).toStrictEqual([])
+  // })
 })
